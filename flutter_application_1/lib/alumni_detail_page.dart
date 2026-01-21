a2930d8f1f1047fdebf6afd436dc39a27872430d
moczygeba <joris.moczygeba@ecole.ensicaen.fr>
2026-01-21 14:04:16 +0100

    Ajout de la page admin avec création, suppression et modification des alumnis

--- flutter_application_1/lib/alumni_detail_page.dart
+++ flutter_application_1/lib/alumni_detail_page.dart
@@ -1,72 +1,167 @@
 import 'package:flutter/material.dart';
-import 'colors.dart'; // Vérifie que ce fichier existe aussi, sinon remplace par Colors.cyan
-import 'alumnis.dart'; // Important : Importe ton modèle
+import 'colors.dart';
+import 'alumnis.dart';
+import 'database_service.dart';
 
-class AlumniDetailPage extends StatelessWidget {
+class AlumniDetailPage extends StatefulWidget {
   final Alumnis alumni;
 
   const AlumniDetailPage({super.key, required this.alumni});
 
   @override
+  State<AlumniDetailPage> createState() => _AlumniDetailPageState();
+}
+
+class _AlumniDetailPageState extends State<AlumniDetailPage> {
+  // Variable pour savoir si on est en mode édition
+  bool _enEdition = false;
+
+  
+  late TextEditingController _posteCtrl;
+  late TextEditingController _entrepriseCtrl;
+  late TextEditingController _villeCtrl;
+
+
+  late String jobActuel;
+  late String entrepriseActuelle;
+  late String villeActuelle;
+
+  @override
+  void initState() {
+    super.initState();
+  
+    jobActuel = widget.alumni.job;
+    entrepriseActuelle = widget.alumni.entreprise;
+    villeActuelle = widget.alumni.ville;
+
+    _posteCtrl = TextEditingController(text: jobActuel);
+    _entrepriseCtrl = TextEditingController(text: entrepriseActuelle);
+    _villeCtrl = TextEditingController(text: villeActuelle);
+  }
+
+  @override
+  void dispose() {
+    // Nettoyage de la mémoire
+    _posteCtrl.dispose();
+    _entrepriseCtrl.dispose();
+    _villeCtrl.dispose();
+    super.dispose();
+  }
+
+  // Fonction pour sauvegarder les changements
+  void _sauvegarder() async {
+
+    await DatabaseService().modifierEleve({
+      "nom": widget.alumni.nom,   
+      "prenom": widget.alumni.prenom,
+      "poste": _posteCtrl.text,
+      "entreprise": _entrepriseCtrl.text,
+      "ville": _villeCtrl.text,
+    });
+
+    setState(() {
+      jobActuel = _posteCtrl.text;
+      entrepriseActuelle = _entrepriseCtrl.text;
+      villeActuelle = _villeCtrl.text;
+      _enEdition = false; // On quitte le mode édition
+    });
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text("Profil mis à jour avec succès !"), backgroundColor: Colors.green),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(alumni.nomComplet),
-        backgroundColor: AppColors.ensiCyan, // Ou Colors.cyan si erreur
+        title: Text(widget.alumni.nomComplet),
+        backgroundColor: AppColors.ensiCyan,
         foregroundColor: Colors.white,
+        actions: [
+        
+          IconButton(
+            icon: Icon(_enEdition ? Icons.save : Icons.edit),
+            tooltip: _enEdition ? "Enregistrer" : "Modifier",
+            onPressed: () {
+              if (_enEdition) {
+                _sauvegarder();
+              } else {
+                setState(() {
+                  _enEdition = true;
+                });
+              }
+            },
+          ),
+        ],
       ),
-      body: SingleChildScrollView( // Ajouté pour éviter que ça dépasse sur petits écrans
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
             CircleAvatar(
               radius: 60,
-              backgroundColor: AppColors.ensiCyan, // Ou Colors.cyan
+              backgroundColor: AppColors.ensiCyan,
               child: Text(
-                alumni.prenom.isNotEmpty ? alumni.prenom[0] : "?",
+                widget.alumni.prenom.isNotEmpty ? widget.alumni.prenom[0] : "?",
                 style: const TextStyle(fontSize: 50, color: Colors.white),
               ),
             ),
             const SizedBox(height: 20),
             Text(
-              alumni.nomComplet, 
-              style: const TextStyle(fontSize: 26, fontWeight: FontWeight.bold)
+              widget.alumni.nomComplet,
+              style: const TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
             ),
             Text(
-              "Promo ${alumni.promo}", 
-              style: const TextStyle(fontSize: 20, color: Colors.grey)
+              "Promo ${widget.alumni.promo}",
+              style: const TextStyle(fontSize: 20, color: Colors.grey),
             ),
             const Divider(height: 40),
-            
-            // Carte d'infos
+
             Card(
               elevation: 2,
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: const Icon(Icons.work, color: Colors.blue),
-                    title: const Text("Poste actuel"),
-                    subtitle: Text("${alumni.job} chez ${alumni.entreprise}"),
-                  ),
-                  const Divider(height: 1),
-                  ListTile(
-                    leading: const Icon(Icons.school, color: Colors.orange),
-                    title: const Text("Filière"),
-                    subtitle: Text(alumni.filiere),
-                  ),
-                  const Divider(height: 1),
-                  ListTile(
-                    leading: const Icon(Icons.location_on, color: Colors.red),
-                    title: const Text("Ville"),
-                    subtitle: Text(alumni.ville),
-                  ),
-                  const Divider(height: 1),
-                  ListTile(
-                    leading: const Icon(Icons.email, color: Colors.green),
-                    title: const Text("Email"),
-                    subtitle: Text("email"),
-                  ),
-                ],
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+          
+                    ListTile(
+                      leading: const Icon(Icons.work, color: Colors.blue),
+                      title: const Text("Poste actuel"),
+                      subtitle: _enEdition
+                          ? TextField(controller: _posteCtrl, decoration: const InputDecoration(border: OutlineInputBorder()))
+                          : Text(jobActuel, style: const TextStyle(fontWeight: FontWeight.bold)),
+                    ),
+                    const Divider(),
+
+                 
+                    ListTile(
+                      leading: const Icon(Icons.business, color: Colors.indigo),
+                      title: const Text("Entreprise"),
+                      subtitle: _enEdition
+                          ? TextField(controller: _entrepriseCtrl, decoration: const InputDecoration(border: OutlineInputBorder()))
+                          : Text(entrepriseActuelle),
+                    ),
+                    const Divider(),
+                    
+
+                    ListTile(
+                      leading: const Icon(Icons.location_on, color: Colors.red),
+                      title: const Text("Ville"),
+                      subtitle: _enEdition
+                          ? TextField(controller: _villeCtrl, decoration: const InputDecoration(border: OutlineInputBorder()))
+                          : Text(villeActuelle),
+                    ),
+                    
+         
+                    const Divider(),
+                    ListTile(
+                      leading: const Icon(Icons.school, color: Colors.orange),
+                      title: const Text("Filière"),
+                      subtitle: Text(widget.alumni.filiere), 
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
